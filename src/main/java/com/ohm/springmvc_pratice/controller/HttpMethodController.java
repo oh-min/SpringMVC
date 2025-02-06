266a4e9a14b7ca680120eaa506a120fa80f3aaac
oh-min <ohyemin96@gmail.com>
2025-02-06 14:22:24 +0900

    Http 메소드란?

---
+++ src/main/java/com/ohm/springmvc_pratice/controller/HttpMethodController.java
@@ -0,0 +1,14 @@
+package com.ohm.springmvc_pratice.controller;
+
+public class HttpMethodController {
+//    HTTP 메소드란?
+//    클라이언트 - 서버 구조에서 요청(request)과 응답(response)이 이루어지는 방식
+//    서버가 수행해야 할 동작을 지정하여 요청을 보내는 방법
+
+//    HTTP 메서드를 사용하는 이유?
+//    리소스와 동작을 분리하기 위함 (HTTP 메소드를 통해 서버가 수행해야 할 동작 지정, URL은 리소스만 식별)
+
+//    종류
+//    GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, CONNECT
+
+}
