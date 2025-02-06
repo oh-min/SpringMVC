36abf253dd5cc870588a91a7cc99937feb3569f9
oh-min <ohyemin96@gmail.com>
2025-02-06 15:25:00 +0900

    @RequestMapping

--- src/main/java/com/ohm/springmvc_pratice/controller/HttpMethodController.java
+++ src/main/java/com/ohm/springmvc_pratice/controller/HttpMethodController.java
@@ -3,43 +3,46 @@ package com.ohm.springmvc_pratice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+//RequestMappign Annotation을 추가
+@RequestMapping("/api")
 @Controller
 public class HttpMethodController {
-//    HTTP 메소드란?
-//    클라이언트 - 서버 구조에서 요청(request)과 응답(response)이 이루어지는 방식
-//    서버가 수행해야 할 동작을 지정하여 요청을 보내는 방법
 
-//    HTTP 메서드를 사용하는 이유?
-//    리소스와 동작을 분리하기 위함 (HTTP 메소드를 통해 서버가 수행해야 할 동작 지정, URL은 리소스만 식별)
+//    Spring 4.3 버전 이전 방식 (현재도 사용 가능)
+//    URL : http://localhost:8080/api/request
+    @RequestMapping(value = "/request", method = RequestMethod.GET)
+    @ResponseBody
+    public String request() {
+        return "RequestMapping GET Method 요청";
+    }
 
-//    종류
-//    GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, CONNECT
-
-    @GetMapping("/api/get")
+//    @GetMapping("/api/get") -> @GetMapping("/get")
+//    URL 은 http://localhost:8080/api/get 으로 동일
+    @GetMapping("/get")
     @ResponseBody
     public String get() {
         return "GET Method 요청";
     }
 
-    @PostMapping("/api/post")
+    @PostMapping("/post")
     @ResponseBody
     public String post() {
         return "POST Method 요청";
     }
 
-    @PutMapping("/api/put")
+    @PutMapping("/put")
     @ResponseBody
     public String put() {
         return "PUT Method 요청";
     }
 
-    @DeleteMapping("/api/delete")
+    @DeleteMapping("/delete")
     @ResponseBody
     public String delete() {
         return "DELETE Method 요청";
     }
 
-    @PatchMapping("/api/patch")
+    @PatchMapping("/patch")
     @ResponseBody
     public String patch() {
         return "PATCH Method 요청";
